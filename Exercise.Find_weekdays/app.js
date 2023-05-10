4f562e8e06e6f69cdadf8e20e9f3355891d72123
Hyboy95 <temilator@gmail.com>
2023-05-10 13:40:18 +0700

    6th commit

--- Exercise.Find_weekdays/app.js
+++ Exercise.Find_weekdays/app.js
@@ -1,3 +1,8 @@
-let weeksDay = ["chủ nhật", "thứ hai", "thứ ba", "thứ tư", "thứ năm", "thứ sáu", "thứ bảy"];
-let today = new Date().getDay();
-console.log(`Hôm nay là ${weeksDay[today]}`);
+let daysOfWeek = ["chủ nhật", "thứ hai", "thứ ba", "thứ tư", "thứ năm", "thứ sáu", "thứ bảy"];
+function getDayOfWeek(date) {
+    let dateObj = new Date(Date.parse(date));
+    return daysOfWeek[dateObj.getDay()]
+}
+
+let date = '5/10/2023';
+console.log(`Ngày ${date} là ${getDayOfWeek(date)}.`)
